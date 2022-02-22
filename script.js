b4d849e2a208ee5b04f4d2aab70ea8bdabf44139
Nataliia <d_tasha@ukr.net>
2022-02-22 22:23:24 +0200

    created a class Employee

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ class Person {
     return "Happy Birthday, let's celebrate'";
   }
 }
+
+class Employee extends Person {
+  #salary = 500;
+  constructor(firstName, lastName, age, birthDayDate, jobPosition) {
+    super(firstName, lastName, age, birthDayDate);
+    this.jobPosition = jobPosition;
+  }
+  getYearSalary() {
+    return #salary * 12;
+  }
+  celebrate () {
+      if (this.birthDayDate===6 || this.birthDayDate===0)
+return "Happy Birthday, let’s celebrate’";
+else
+return "Happy Birthday, but I need to work";
+  }
+  
+}
