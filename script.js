f1e6657d882dea54163dbccce0ff74f8b637faab
Nataliia <d_tasha@ukr.net>
2022-02-22 22:09:09 +0200

    created a class Person

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+// Person, который будет иметь следующие поля:
+class Person {
+  constructor(firstName, lastName, age, birthDayDate) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.birthDayDate = birthDayDate;
+  }
+
+  celebrate() {
+    return "Happy Birthday, let's celebrate'";
+  }
+}
