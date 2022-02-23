f067e7fce93ed0a4034b901606add0dbf6b5412a
Nataliia <d_tasha@ukr.net>
2022-02-23 21:13:35 +0200

    created a class instance

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-// Person, который будет иметь следующие поля:
+
 class Person {
   constructor(firstName, lastName, age, birthDayDate) {
     this.firstName = firstName;
@@ -20,13 +20,27 @@ class Employee extends Person {
     this.jobPosition = jobPosition;
   }
   getYearSalary() {
-    return #salary * 12;
+    return this.#salary * 12;
   }
-  celebrate () {
-      if (this.birthDayDate===6 || this.birthDayDate===0)
-return "Happy Birthday, let’s celebrate’";
-else
-return "Happy Birthday, but I need to work";
+  celebrate() {
+    this.date = new Date(this.birthDayDate);
+    this.newDay = this.date.getDay();
+    if (this.newDay === 6 || this.newDay === 0)
+      return "Happy Birthday, let’s celebrate’";
+    else return "Happy Birthday, but I need to work";
   }
-  
 }
+
+const person = new Person("ivan", "ivanov", 25, "1989-02-03");
+console.log(person);
+console.log(person.celebrate());
+
+const employee = new Employee("petr", "petrov", 30, "2022-02-12", "ingineer");
+console.log(employee);
+console.log(employee.getYearSalary());
+console.log(employee.celebrate());
+
+const employee1 = new Employee("Marta", "Super", 26, "2022-02-09", "mother");
+console.log(employee1);
+console.log(employee1.getYearSalary());
+console.log(employee1.celebrate());
